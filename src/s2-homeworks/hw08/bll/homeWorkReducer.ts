a53179178be30cf54c3fb8f864ced7fae1bd0d33
djonnybravo <djonnybravo29@gmail.com>
2023-02-23 00:34:50 +0300

    done hw8 types, and logics

--- src/s2-homeworks/hw08/bll/homeWorkReducer.ts
+++ src/s2-homeworks/hw08/bll/homeWorkReducer.ts
@@ -4,15 +4,18 @@ type ActionType =
     | { type: 'sort'; payload: 'up' | 'down' }
     | { type: 'check'; payload: number }
 
-export const homeWorkReducer = (state: any, action: any): any => { // need to fix any
+export const homeWorkReducer = (state: UserType[], action: ActionType): UserType[] => { // need to fix any
     switch (action.type) {
         case 'sort': { // by name
-
-            return state // need to fix
+            let copyState = [...state]
+            return action.payload === "up"
+                ? copyState.sort((a,b) => a.name.localeCompare(b.name))
+                : copyState.sort((a,b) => b.name.localeCompare(a.name))
+            // need to fix
         }
         case 'check': {
-
-            return state // need to fix
+            let copyState = [...state]
+            return copyState.filter(u => u.age > action.payload)
         }
         default:
             return state
