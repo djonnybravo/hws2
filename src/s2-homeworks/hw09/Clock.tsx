145dbda7bbb800875555e34c5aadbed65a08d73e
djonnybravo <djonnybravo29@gmail.com>
2023-05-24 18:17:30 +0300

    fix: data display HW9

--- src/s2-homeworks/hw09/Clock.tsx
+++ src/s2-homeworks/hw09/Clock.tsx
@@ -30,11 +30,11 @@ function Clock() {
 
 
     const stringTime = date.toLocaleTimeString() || <br/> // часы24:минуты:секунды (01:02:03)/(23:02:03)/(24:00:00)/(00:00:01) // пишут студенты
-    const stringDate =  date.toLocaleDateString("en", { month: 'long', day: 'numeric'}) || <br/> // день.месяц.год (01.02.2022) // пишут студенты, варианты 01.02.0123/01.02.-123/01.02.12345 не рассматриваем
+    const stringDate =  date.toLocaleDateString("ru", {dateStyle: "short", }) || <br/> // день.месяц.год (01.02.2022) // пишут студенты, варианты 01.02.0123/01.02.-123/01.02.12345 не рассматриваем
 
     // день недели на английском, месяц на английском (https://learn.javascript.ru/intl#intl-datetimeformat)
-    const stringDay = date.toLocaleString("en", {day: '2-digit',  }) || <br/> // пишут студенты
-    const stringMonth = date.toLocaleString("en", {month: '2-digit' }) || <br/> // пишут студенты
+    const stringDay = date.toLocaleString("en", { weekday: "long"})|| <br/> // пишут студенты
+    const stringMonth = date.toLocaleString("en", {month: 'long' }) || <br/> // пишут студенты
 
     return (
         <div className={s.clock}>
