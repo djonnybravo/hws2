1b84a792113739215a1350d02c9aee22cadc04c7
djonnybravo <djonnybravo29@gmail.com>
2023-05-28 21:44:32 +0300

    fix: HW10

--- src/s2-homeworks/hw10/HW10.tsx
+++ src/s2-homeworks/hw10/HW10.tsx
@@ -16,11 +16,12 @@ import {Loader} from './Loader'
 const HW10 = () => {
     // useSelector, useDispatch // пишет студент
     const isLoading = useSelector<AppStoreType, boolean>(state => state.loading.isLoading)
+    const dispatch = useDispatch()
     const setLoading = () => { // пишет студент // показать крутилку на 1,5 секунд
         // dispatch
-        store.dispatch(loadingAC(true))
+        dispatch(loadingAC(true))
         setTimeout(() => {
-            store.dispatch(loadingAC(true))
+           dispatch(loadingAC(false))
         }, 1500)
         // setTimeout
     }
