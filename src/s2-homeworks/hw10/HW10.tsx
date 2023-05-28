2f6d6ee50357d12df23173fd23217aebdccb3a82
djonnybravo <djonnybravo29@gmail.com>
2023-05-28 21:39:48 +0300

    feat: HW10

--- src/s2-homeworks/hw10/HW10.tsx
+++ src/s2-homeworks/hw10/HW10.tsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import {useDispatch, useSelector} from 'react-redux'
-import {AppStoreType} from './bll/store'
+import store, {AppStoreType} from './bll/store'
 import {loadingAC} from './bll/loadingReducer'
 import SuperButton from '../hw04/common/c2-SuperButton/SuperButton'
 import s2 from '../../s1-main/App.module.css'
@@ -15,11 +15,13 @@ import {Loader} from './Loader'
 
 const HW10 = () => {
     // useSelector, useDispatch // пишет студент
-    const isLoading = false
-
+    const isLoading = useSelector<AppStoreType, boolean>(state => state.loading.isLoading)
     const setLoading = () => { // пишет студент // показать крутилку на 1,5 секунд
         // dispatch
-
+        store.dispatch(loadingAC(true))
+        setTimeout(() => {
+            store.dispatch(loadingAC(true))
+        }, 1500)
         // setTimeout
     }
 
