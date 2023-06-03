d0cd686054f8f802513fc8fa38d12f5a28982f49
djonnybravo <djonnybravo29@gmail.com>
2023-06-03 19:38:40 +0300

    feat: add styles for slider

--- src/s2-homeworks/hw11/common/c7-SuperRange/SuperRange.tsx
+++ src/s2-homeworks/hw11/common/c7-SuperRange/SuperRange.tsx
@@ -5,7 +5,10 @@ const SuperRange: React.FC<SliderProps> = (props) => {
     return (
         <Slider
             sx={{ // стили для слайдера // пишет студент
-                
+                width: 300,
+                color: '#00CC22',
+                height: 3,
+                padding: '13px 0',
             }}
             {...props} // отдаём слайдеру пропсы если они есть (value например там внутри)
         />
